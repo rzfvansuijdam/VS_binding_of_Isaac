1158ce36e1a43a6cc9814ac69d57c852e599b212
rzfvansuijdam <55277122+rzfvansuijdam@users.noreply.github.com>
2020-11-19 09:49:25 +0100

    added health

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerHealth : Health
+{
+
+
+    private void Start()
+    {
+        starthealth = 100;
+        health = starthealth;
+        alive = true;
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(25);
+            Debug.Log("player is taking damage");
+        }
+
+        if (health <= 0)
+        {
+            Debug.Log("dead");
+        }
+    }
+
+    public void Update()
+    {
+        
+      
+
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
