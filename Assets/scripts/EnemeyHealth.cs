0909879fd67f1241f23d004f47d4f9cd54874256
rzfvansuijdam <55277122+rzfvansuijdam@users.noreply.github.com>
2020-11-23 10:29:52 +0100

    pushed to main

---
+++ Assets/scripts/EnemeyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyHealth : Health
+{
+
+    void Start()
+    {
+        starthealth = 100;
+        health = starthealth;
+
+
+    }
+
+    private void Update()
+    {
+
+
+
+        if (health <= 0) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("bullet"))
+        { 
+            TakeDamage(25);
+        }
+    }
+}
+
+
