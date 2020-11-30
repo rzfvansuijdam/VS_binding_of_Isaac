e572e95ab2345f40c4a4b452a42db22206ec3d8e
EmileDavidson <Emiledavidson395@gmail.com>
2020-11-30 11:40:56 +0100

    init

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealth : Health
 {
+    public UnityEvent damageTakeEvent;
+
     private void Start()
     {
         starthealth = 100;
@@ -14,18 +17,13 @@ public class PlayerHealth : Health
     public override void RemoveHealth(float amount)
     {
         base.RemoveHealth(amount);
-        updateUI();
+        damageTakeEvent.Invoke();
     }
 
     public override void AddHealth(float amount)
     {
         base.AddHealth(amount);
-        updateUI();
-    }
-
-    public void updateUI()
-    {
-        //show new health in ui
+        damageTakeEvent.Invoke();
     }
 
     public override void Die()
