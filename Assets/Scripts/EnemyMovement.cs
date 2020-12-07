1e0827f07cf1ac9c735921bb8331ac7ae6b67eec
riley <rzfvansuijdam@gmail.com>
2020-12-07 15:12:51 +0100

    added enemy movemnt

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float accelerationTime = 2f;
+    public float maxSpeed = 5f;
+    private Vector2 movement;
+    private float timeLeft;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            timeLeft += accelerationTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * maxSpeed);
+    }
+}
