0805a7bea79e53c5edb36f3b99489a5bedcac10d
riley <rzfvansuijdam@gmail.com>
2020-12-11 11:54:56 +0100

    uuggh

--- Assets/Scripts/Health/EnemyHealth.cs
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -1,12 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyHealth : Health
 {
+    public UnityEvent damageTakeEvent;
+
     private void Start()
     {
-        starthealth = 100;
+        starthealth = 4;
         health = starthealth;
+    }
+
+    public void Update() {
+
+
+    }
+
+   public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "bullet")
+            RemoveHealth(1);
+        Debug.Log("got em");
+    }
+
+    public override void RemoveHealth(float amount)
+    {
+        base.RemoveHealth(amount);
+        damageTakeEvent.Invoke();
+    }
+
+    public override void Die()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
